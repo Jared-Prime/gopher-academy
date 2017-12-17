ac521f0df3bddb1a97436e071207d2c9aca8296d
Jared Davis <jared@haiq.us>
2017-12-16 18:14:00 -0600

    extract port setting into environment variable

--- kubernetes/main.go
+++ kubernetes/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 import "./handlers"
@@ -10,8 +11,13 @@ import "./handlers"
 func main() {
 	log.Print("Starting the service...")
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("No port set!")
+	}
+
 	router := handlers.Router()
 
-	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Print("The service is ready to listen and serve on port ", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
