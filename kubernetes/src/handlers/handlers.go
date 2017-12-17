c528fdf6ae1e869a8a9de8d8909413ec12b03560
Jared Davis <jared@haiq.us>
2017-12-16 19:01:29 -0600

    add health and readiness checks to router

--- kubernetes/src/handlers/handlers.go
+++ kubernetes/src/handlers/handlers.go
@@ -1,9 +1,26 @@
 package handlers
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"sync/atomic"
+	"time"
+)
 
 func Router() *mux.Router {
+	isReady := &atomic.Value{}
+	isReady.Store(false)
+
+	go func() {
+		log.Printf("readiness set to false by default")
+		time.Sleep(10 * time.Second)
+		isReady.Store(true)
+		log.Printf("readiness set to true after 10 seconds")
+	}()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/home", home).Methods("GET")
+	r.HandleFunc("/health", health)
+	r.HandleFunc("/ready", ready(isReady))
 	return r
 }
