d37003b9d6232d8c0e1bbe8fd9d1ba062a418bd1
Jared Davis <jared@haiq.us>
2017-12-19 13:43:59 -0600

    weekly forecast interface
    wraps the protocol buffer's interface

---
+++ grpc/weather/weekly_forecast.go
@@ -0,0 +1,8 @@
+package weather
+
+import "github.com/go-redis/cache"
+
+type WeeklyForecast interface {
+  GetForecastWeek(*GetForecastWeekRequest) (*ForecastWeekInfo, error)
+  CachedWeeklyForecast(*GetForecastWeekRequest, *cache.Codec) (*ForecastWeekInfo, error)
+}
