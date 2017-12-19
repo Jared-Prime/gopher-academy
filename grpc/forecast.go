9c831273e73f6b28d448adbb76314ded72b5f253
Jared Davis <jared@haiq.us>
2017-12-19 13:42:03 -0600

    remove lesson plan
    not going to use it directly. I'm going to build out my own example(s)

---
+++ grpc/forecast.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+  "github.com/go-redis/cache"
+  "github.com/go-redis/redis"
+	"fmt"
+	"log"
+	"os"
+
+	weather "github.com/jared-prime/gopher-academy/grpc/weather"
+)
+
+
+
+var (
+	wundergroundApiKeyId  string
+	redisHost             string
+	redisCache            *cache.Codec
+)
+
+func init() {
+	wundergroundApiKeyId = os.Getenv("WEATHER_UNDERGROUND_API_KEY")
+	if wundergroundApiKeyId == "" {
+		log.Fatal("$WEATHER_UNDERGROUND_API_KEY not set")
+	}
+
+	redisHost = os.Getenv("REDIS_HOSTNAME")
+	if redisHost == "" {
+		log.Fatal("$REDIS_HOSTNAME not set")
+	}
+
+	redisCache = &cache.Codec{
+		Redis: redis.NewRing(&redis.RingOptions{
+			Addrs: map[string]string{
+				"redis1": fmt.Sprintf("%s:6379", redisHost),
+			},
+		}),
+		Marshal:   func(v interface{}) ([]byte, error) { return json.Marshal(v) },
+		Unmarshal: func(b []byte, v interface{}) error { return json.Unmarshal(b, v) },
+	}
+}
+
+func main() {
+  weatherUnderground, err := weather.NewweatherChannelApiClient(wundergroundApiKeyId)
+  if err != nil {
+    log.Fatal(err)
+  }
+}
