68269b4f843a3f0032f2b033f69191a1297067f4
Jared Davis <jared@haiq.us>
2017-12-20 09:28:45 -0600

    add weather underground response parsing

--- grpc/weather/weather_underground_api_response.go
+++ grpc/weather/weather_underground_api_response.go
@@ -1,40 +1,41 @@
 package weather
 
-type WeatherUndergroundApiForecast struct {
-  SimpleForecasts []*WeatherUndergroundSimpleForecast `json:"simpleforecast"`
+type WeatherUndergroundApiForecastResponse struct {
+	Forecast WeatherUndergroundForecast `json:"forecast"`
 }
 
-type WeatherUndergroundSimpleForecast struct {
-  ForecastDays []*WeatherUndergroundForecastDay `json:"forecastday"`
+type WeatherUndergroundForecast struct {
+	Simple struct {
+		Days []WeatherUndergroundForecastDay `json:"forecastday"`
+	} `json:"simpleforecast"`
 }
 
 type WeatherUndergroundForecastDay struct {
-  WeatherUndergroundForecastDate `json:"date"`
-  WeatherUndergroundForecastHigh `json:"high"`
-  WeatherUndergroundForecastLow  `json:"low"`
-  Conditions                     string `json:"conditions`
+	Date struct {
+		Weekday string `json:"weekday"`
+	} `json:"date"`
+
+	High struct {
+		Fahrenheit string `json:"fahrenheit"`
+		Celsius    string `json:"celsius"`
+	} `json:"high"`
+
+	Low struct {
+		Fahrenheit string `json:"fahrenheit"`
+		Celsius    string `json:"celsius"`
+	} `json:"low"`
+
+	Conditions string `json:"conditions"`
 }
 
-type WeatherUndergroundForecastDate struct {
-  Weekday string `json:"weekday"`
+func (resp WeatherUndergroundApiForecastResponse) Forecasts() []WeatherUndergroundForecastDay {
+	return resp.Forecast.Simple.Days
 }
 
-type WeatherUndergroundForecastHigh struct {
-  Fahrenheit int32 `json:"fahrenheit"`
-  Celsius    int32 `json:"celsius`
+func (forecast WeatherUndergroundForecastDay) DayOfWeek() int32 {
+	return DayOfWeek_value[forecast.Date.Weekday[0:3]]
 }
 
-type WeatherUndergroundForecastLow struct {
-  Fahrenheit int32 `json:"fahrenheit"`
-  Celsius    int32 `json:"celsius`
-}
-
-var WundergroundDays_Number = map[string]DayOfWeek{
-  "Sunday":    DayOfWeek_Sun,
-  "Monday":    DayOfWeek_Mon,
-  "Tuesday":   DayOfWeek_Tue,
-  "Wednesday": DayOfWeek_Wed,
-  "Thursday":  DayOfWeek_Thu,
-  "Friday":    DayOfWeek_Fri,
-  "Saturday":  DayOfWeek_Sat,
+func (forecast WeatherUndergroundForecastDay) HighTemperature() string {
+	return forecast.High.Fahrenheit
 }
---
+++ grpc/weather/weather_underground_api_response_test.go
@@ -0,0 +1,64 @@
+package weather
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"testing"
+)
+
+func Subject(t *testing.T) WeatherUndergroundApiForecastResponse {
+	raw, err := ioutil.ReadFile("example.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	subject := WeatherUndergroundApiForecastResponse{}
+
+	err = json.Unmarshal(raw, &subject)
+	if err != nil {
+		t.Error(err)
+	}
+
+	return subject
+}
+
+func TestWeatherUndergroundApiForecastResponseForecasts(t *testing.T) {
+	assert := assert.New(t)
+	subject := Subject(t)
+	
+	assert.Equal(10, len(subject.Forecasts()))
+}
+
+func TestWeatherUndergroundForecastDayDayOfWeek(t *testing.T) {
+	assert := assert.New(t)
+	subject := Subject(t)
+
+	for i, forecast_day := range subject.Forecasts() {
+		expected := (i+2)%7
+
+		assert.Equal(int32(expected), forecast_day.DayOfWeek())
+	}
+}
+
+func TestWeatherUndergroundForecastDayHighTemperature(t *testing.T) {
+	assert := assert.New(t)
+	subject := Subject(t)
+
+	expectations := map[int]string{
+		0:"50",
+		1:"33",
+		2:"38",
+		3:"39",
+		4:"32",
+		5:"28",
+		6:"22",
+		7:"14",
+		8:"19",
+		9:"22",
+	}
+
+	for i, forecast_day := range subject.Forecasts() {
+		assert.Equal(expectations[i], forecast_day.HighTemperature())
+	}
+}
